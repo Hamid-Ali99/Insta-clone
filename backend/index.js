b9b0d924e0dfc91c190d6e33be5f52c15439334d
Hamid <hamidali006789@gmail.com>
2024-08-13 17:37:29 +0500

    add message controller

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,7 @@ import cookieParser from "cookie-parser";
 import connectDB from "./utils/db.js";
 import userRouter from "./routes/user.route.js";
 import postRouter from "./routes/post.route.js";
+import messageRoute from "./routes/message.route.js";
 
 dotenv.config();
 
@@ -28,6 +29,7 @@ app.use(cookieParser());
 // our apis
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/post", postRouter);
+app.use("/api/v1/message", messageRoute);
 
 // app.get("/", (req, res) => {
 //   res.status(200).json({
